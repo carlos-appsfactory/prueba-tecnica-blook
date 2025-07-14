7c0de1515a5e3aaea738d5d97511090e52c38906
Carlos <carlos@appsfactory.com>
2025-07-14 12:20:34 +0200

    First version of the back

---
+++ api/index.js
@@ -0,0 +1,131 @@
+import fs from 'fs';
+import path from 'path';
+import express from "express";
+import cors from 'cors';
+import PDFDocument from 'pdfkit';
+import { body, validationResult }  from 'express-validator';
+import { tmpdir } from 'os';
+import { randomUUID } from 'crypto';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const certificateValidations = [
+  body('address')
+    .notEmpty().withMessage({ text: 'La dirección es obligatoria', code: 1000 })
+    .bail()
+    .isString().withMessage({ text: 'La dirección debe ser un texto', code: 1001 }),
+
+  body('usableArea')
+    .notEmpty().withMessage({ text: 'La superficie útil es obligatoria', code: 1002 })
+    .bail()
+    .isFloat({ gt: 0 }).withMessage({ text: 'La superficie útil debe ser un número positivo', code: 1003 }),
+
+  body('yearBuilt')
+    .notEmpty().withMessage({ text: 'El año de construcción es obligatorio', code: 1004 })
+    .bail()
+    .isInt({ min: 1800, max: new Date().getFullYear() }).withMessage({ text: 'El año de construcción debe ser un número entre 1800 y el año actual', code: 1005 }),
+
+  body('propertyType')
+    .notEmpty().withMessage({ text: 'El tipo de propiedad es obligatorio', code: 1006 })
+    .bail()
+    .isIn(['apartment', 'house', 'commercial', 'office', 'land']).withMessage({ text: 'Tipo de propiedad no válido', code: 1007 }),
+
+  body('energyRating')
+    .notEmpty().withMessage({ text: 'El certificado energético es obligatorio', code: 1008 })
+    .bail()
+    .isIn(['A', 'B', 'C', 'D', 'E', 'F', 'G']).withMessage({ text: 'Certificado energético no válido', code: 1009 }),
+];
+
+function generateFormatedDate(date = new Date()){
+ const dateOptions = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+
+  const hourOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  };
+
+  const dateText = new Intl.DateTimeFormat('es-ES', dateOptions).format(date);
+  const hourText = new Intl.DateTimeFormat('es-ES', hourOptions).format(date);
+
+  return `${dateText.charAt(0).toUpperCase() + dateText.slice(1)} a las ${hourText}`;
+}
+
+app.get("/certificate/:fileName", (req, res) =>{
+  const { fileName } = req.params;
+  const filePath = path.join(tmpdir(), `${fileName}.pdf`);
+
+  res.download(filePath, err => {
+    if (err) {
+      console.error('Error al enviar archivo:', err);
+      return res.status(404).json({
+        success: false,
+        errors: [
+          {
+            "message": `Error al enviar archivo: ${err}`,
+            "code": 2000
+          }
+        ]
+      });
+    }
+  });
+});
+
+app.post("/certificate", certificateValidations, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const formatted = errors.array().map(err => ({
+      field: err.param,
+      message: err.msg.text,
+      code: err.msg.code,
+    }));
+
+    console.log(formatted)
+
+    return res.status(400).json({ success: false, errors: formatted });
+  }
+
+  const data = req.body;
+  const pdf = new PDFDocument();
+  const fileName = `certificate-${randomUUID()}`;
+  const filePath = path.join(tmpdir(), `${fileName}.pdf`);
+
+  pdf.pipe(fs.createWriteStream(filePath));
+  
+  pdf.fontSize(40)
+    .font('Helvetica-Bold')
+    .text('Certificado Energético', {
+      align: 'center',
+      underline: true
+    });
+  
+  pdf.fontSize(25).text(generateFormatedDate(), { align: 'center' });
+
+  pdf.moveDown().fontSize(14);
+
+  Object.entries(data).forEach(([key, value]) => {
+    pdf.font('Helvetica-Bold').text(`${key}: `, { continued: true });
+    pdf.font('Helvetica').text(`${value}`);
+  });
+
+  pdf.end();
+
+  // TODO: Firmar el PDF con un certificado digital
+
+  return res.json({ success: true, data: { fileName: fileName }});
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
